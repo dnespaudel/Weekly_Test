d4dbd517d7f43ecaca442ce5b6ebea932fe1d9bf
dnespaudel <dnespaudel10@gmail.com>
2022-02-26 13:12:40 +0545

    Week 1 Test - Completed

--- emailScrap.py
+++ emailScrap.py
@@ -9,8 +9,8 @@ non_human_email_count = 0
 for line in file:
     words = line.split(' ')
     for word in words:
-        human_mail = re.findall(r"[a-z]+\.+[a-z0-9]+@[\w.]+[a-z]{2,3}", word)
-        non_human_mail = re.findall(r"^[\w]{2,8}@[\w.]+[a-z]{2,3}", word)
+        human_mail = re.findall(r"[a-z]+\.[a-z0-9]+@[\w]+\.[a-z]{2,3}", word)
+        non_human_mail = re.findall(r"^[\w]{2,8}@[\w]+\.[a-z]{2,3}", word)
         if human_mail:
             human_email_count += 1
             d[human_mail[0]] = {'Occurrence': human_email_count, 'EmailType': 'Human'}
@@ -18,6 +18,6 @@ for line in file:
         if non_human_mail:
             non_human_email_count += 1
             d[non_human_mail[0]] = {'Occurrence': non_human_email_count, 'EmailType': 'Non-Human'}
-
-with open('result.json', 'w', encoding='utf-8') as f:
-    json.dump(d, f, ensure_ascii=False, indent=4)
+print(d)
+# with open('result.json', 'w', encoding='utf-8') as f:
+#     json.dump(d, f, ensure_ascii=False, indent=4)
